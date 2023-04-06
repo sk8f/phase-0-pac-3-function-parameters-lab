28041ad85f7c074be002c4b6b5645913e5fb472c
Jay <skatefrosty@outlook.com>
2023-04-06 06:42:08 -0400

    LAB

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ function introductionWithLanguage(name, language){
     return (`Hi, my name is ${name} and I am learning to program in ${language}.`);
 }
 
-function introductionWithLanguageOptional(name, language='Javascript'){
+function introductionWithLanguageOptional(name, language='JavaScript'){
     return (`Hi, my name is ${name} and I am learning to program in ${language}.`)
 }
